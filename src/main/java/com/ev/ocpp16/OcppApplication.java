a39f70d4bf8c16c6c00c2db9dd46dc4bd8f27c09
jh-chch <jhchoig9@gmail.com>
2024-11-20 23:47:47 +0900

    first commit

---
+++ src/main/java/com/ev/ocpp16/OcppApplication.java
@@ -0,0 +1,13 @@
+package com.ev.ocpp16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charge/ocpp/OcppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charge.ocpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
