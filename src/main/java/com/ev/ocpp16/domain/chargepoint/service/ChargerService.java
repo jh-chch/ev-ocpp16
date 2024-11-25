4eba325d5a53c797c3db7f79b24f4135d7d4d675
jh-chch <jhchoig9@gmail.com>
2024-11-25 13:49:18 +0900

    feat: 서버 시작 시 모든 충전기 연결 상태 초기화 기능 구현
    - 서버 재시작 시 모든 충전기의 연결 상태를 DISCONNECTED로 판단하고 초기화함

--- src/main/java/com/ev/ocpp16/domain/chargepoint/service/ChargerService.java
+++ src/main/java/com/ev/ocpp16/domain/chargepoint/service/ChargerService.java
@@ -1,17 +1,28 @@
 package com.ev.ocpp16.domain.chargepoint.service;
 
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
+import com.ev.ocpp16.websocket.entity.enums.ChgrConnSt;
 import com.ev.ocpp16.websocket.repository.ChargerRepository;
 
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Chgr: Charger
+ * Conn: Connection
+ * St: Status
+ */
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ChargerService {
     private final ChargerRepository chargerRepository;
 
     public boolean isChgrActiveTrue(Long chgrId) {
         return chargerRepository.findByIdAndIsActiveTrue(chgrId).isPresent();
+    }
+
+    public void updateAllChgrConnSt(ChgrConnSt chgrConnSt) {
+        chargerRepository.updateAllChgrConnSt(chgrConnSt);
     }
 }
--- src/main/java/com/ev/ocpp16/websocket/OcppWebSocketHandler.java
+++ src/main/java/com/ev/ocpp16/websocket/OcppWebSocketHandler.java
@@ -6,11 +6,25 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.ev.ocpp16.domain.chargepoint.service.ChargerService;
+import com.ev.ocpp16.websocket.entity.enums.ChgrConnSt;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
+@RequiredArgsConstructor
 public class OcppWebSocketHandler extends TextWebSocketHandler {
+
+    private final ChargerService chargerService;
+
+    @PostConstruct
+    public void initializeChargerConnectionStatus() {
+        // 모든 충전기 상태 DISCONNECTED 상태로 변경
+        chargerService.updateAllChgrConnSt(ChgrConnSt.DISCONNECTED);
+    }
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
