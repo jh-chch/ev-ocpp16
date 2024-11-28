3d9f8077a913f13266c225660b56084a923f8a15
jh-chch <jhchoig9@gmail.com>
2024-11-28 15:01:57 +0900

    feat: Action Logging AOP 기능 구현
    - ActionHandler 구현체들의 request, response 로깅 기능

---
+++ src/main/java/com/ev/ocpp16/websocket/aop/LoggingAspect.java
@@ -0,0 +1,27 @@
+package com.ev.ocpp16.websocket.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+    @Before("execution(* com.ev.ocpp16.websocket..ActionHandler+.handleAction(..))")
+    public void logMethodEntry(JoinPoint joinPoint) {
+        String shortString = joinPoint.getSignature().toShortString();
+        Object[] args = joinPoint.getArgs();
+        log.info("====> [{}] args: {}", shortString, args);
+    }
+
+    @AfterReturning(pointcut = "execution(* com.ev.ocpp16.websocket..ActionHandler+.handleAction(..))", returning = "result")
+    public void logMethodExit(JoinPoint joinPoint, Object result) {
+        String shortString = joinPoint.getSignature().toShortString();
+        log.info("<==== [{}] return: {}", shortString, result);
+    }
+}
